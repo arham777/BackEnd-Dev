aa7b211b5b44445a448034be2c79c58c9e00b16c
70137556 <70137556@uol.pk>
2024-10-16 13:32:56 +0500

    lab4

---
+++ lab4/format_string.py
@@ -0,0 +1,13 @@
+
+name=input("Enter name ")
+number=len(name)*3
+print("Hello {}, your lucky number is {}".format(name,number))
+name=input("Enter name ")
+number=len(name)*3
+print("Hello {}, your lucky number is {}".format(name,number))
+
+name=input("Enter name ")
+number=len(name)*3
+print(f"Hello {name}, your lucky number is {number}")
+
+
---
+++ lab4/list.py
@@ -0,0 +1,22 @@
+
+#mutable
+# 1 append => adds data to end of the list
+
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.append("Kiwi")
+print(fruits)
+
+# 2 insert => add data to index
+
+fruits.insert(0, "Orange")
+print(fruits)
+
+# 3 remove => remove data from first occurrence within the list
+
+fruits.remove("Melon")
+print(fruits)
+
+# 4 pop = get the element at the index and remove from the list
+
+fruits.pop(2)
+print(fruits)
