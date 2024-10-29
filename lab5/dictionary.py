8022c7916f80810271fabd6f68e32a0e26d0cca2
70137556 <70137556@uol.pk>
2024-10-29 14:57:23 +0500

    lab5

---
+++ lab5/dictionary.py
@@ -0,0 +1,21 @@
+file_counts = { "jpg":10 , "txt":14, "csv": 12, "py": 23 }
+
+# for extension in file_counts:
+#     print(extension)
+
+
+
+for ext, amount in file_counts.items():
+    # print("There are {} files with the .{} extensions ".format( amount, ext))
+    print(f"There are {amount} files with the .{ext} extensions ")
+
+
+
+# cool_beasts= {"octopus":"tentacles" , "dolphines":"fins" , "rhinos":"horns" }
+
+# for animal , breed in cool_beasts.items():
+#     print(f"{animal} have {breed}")
+
+    # print( " {} have {} ".format(animal, breed))
+
+
